1f6f144e6520540d0c8bca0666c9835aaae681d9
Bonnie Lin <bonbonnieuuu@gmail.com>
2022-03-15 20:58:46 +0800

    20220315

---
+++ BankListApi/App_Start/AutofacConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Autofac;
+using Autofac.Integration.Mvc;
+using System.Web.Mvc;
+
+namespace BankListApi.App_Start
+{
+    public static class AutofacConfig
+    {
+        public static void Register()
+        {
+            //容器建立者
+            var builder = new ContainerBuilder();
+            builder.RegisterModule<AutofacModule>();
+
+            //建立容器
+            IContainer container = builder.Build();
+
+            //解析容器內的型別
+            AutofacDependencyResolver resolver = new AutofacDependencyResolver(container);
+
+            //建立相依解析器
+            DependencyResolver.SetResolver(resolver);
+        }
+    }
+}
---
+++ BankListApi/App_Start/AutofacModule.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Autofac;
+
+namespace BankListApi.App_Start
+{
+    public class AutofacModule:Autofac.Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            var types = ThisAssembly.GetTypes();
+            string sNamespace = ThisAssembly.GetName().Name;
+            var nsArray = new List<string>
+            {
+                $"{sNamespace}.Controllers",
+                $"{sNamespace}.Services",
+                $"{sNamespace}.Commands",
+                $"{sNamespace}.Repositories"
+            };
+
+            builder.RegisterTypes(types.Where(x =>
+                    x.Namespace != null && nsArray.Exists(ns => x.Namespace.StartsWith(ns))).ToArray())
+                .InstancePerLifetimeScope();
+        }
+    }
+}
